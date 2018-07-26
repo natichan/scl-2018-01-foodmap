3d8f2bf81aa41c4b7a23251eaadebec504bbbfcb
Natalia <nataliamh.88@gmail.com>
2018-07-25 23:00:29 -0400

    Geo localización con restaurants santiago

---
+++ src/app.js
@@ -0,0 +1,41 @@
+// api key: AIzaSyCYQW0o0YMY9NDMjOI3ur2BPA60bLU08xM
+var map;
+var infowindow;
+
+  function initMap() {
+    var pyrmont = {lat: -33.4375545, lng: -70.65048960000001};
+
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: pyrmont,
+      zoom: 15
+    });
+
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+      location: pyrmont,
+      radius: 500,
+      type: ['restaurant']
+    }, callback);
+  }
+
+  function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
+    }
+  }
+
+  function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent(place.name);
+      infowindow.open(map, this);
+    });
+  }
---
+++ src/js/splash.js
@@ -0,0 +1,9 @@
+window.onload=timeout;
+function timeout() {
+    window.setTimeout("redirect()", 3000)
+}
+
+function redirect() {
+    window.location="main.html"
+        return
+}
